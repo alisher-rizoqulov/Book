0e99e5da5040f6daa7c28d1425fe1cb2b663ad9c
Alisher_Programing <alisherrizoqulov90@gmail.com>
2024-10-18 11:16:05 +0500

    Update Book.py

--- Book.py
+++ Book.py
@@ -9,8 +9,9 @@ class BOOK:
     def writee(self, ptr):
         ptr.write(f"{self.id},{self.name},{self.author},{self.count},{self.price}\n")
 
-    @staticmethod
+  
     def read():
+        print("Mavjud kitoblar:")
         with open("yozish.txt", "r") as ptr:
             print(ptr.read())
 
@@ -19,18 +20,19 @@ class BOOK:
             lines = ptr.readlines()
 
         updated_lines = []
-        found = False
+        lampochka = False
         for line in lines:
             data = line.strip().split(',')
             if int(data[0]) == idd:
-                data[3] = str(int(data[3]) - 1)  # Kitob miqdorini 1 taga kamaytirish
+                data[3] = str(int(data[3]) - 1)  
                 print(f"Kitob ID {idd} uchun miqdor 1 donaga kamaytirildi!")
-                found = True
+                lampochka = True
             updated_lines.append(','.join(data) + '\n')
 
-        if found:
+        if lampochka:
             with open("yozish.txt", "w") as ptr:
                 ptr.writelines(updated_lines)
+                
         else:
             print("Kitob topilmadi.")
 
@@ -39,34 +41,28 @@ class BOOK:
             lines = ptr.readlines()
 
         updated_lines = []
-        found = False
+        lampochka = False
         for line in lines:
             data = line.strip().split(',')
             if int(data[0]) != idd:
-                updated_lines.append(line)  # O'chirilmaydigan kitoblarni qayta yozamiz
+                updated_lines.append(line) 
             else:
-                found = True
+                lampochka = True
 
-        if found:
+        if lampochka:
             with open("yozish.txt", "w") as ptr:
                 ptr.writelines(updated_lines)
             print(f"Kitob ID {idd} o'chirildi!")
         else:
             print("Kitob topilmadi.")
-
-# Ma'lumotlarni faylga yozish
 with open("yozish.txt", "a") as ptr:
     for i in range(1, int(input("Filega nechta malumot kiritasiz: ")) + 1):
         print(f"{i} - Kitob:")
         x = BOOK(int(input("ID: ")), input("Nomi: "), input("Muallif: "), int(input("Nechta: ")), int(input("Narxi: ")))
         x.writee(ptr)
-
-# Fayldan o'qish
 BOOK.read()
-
-# Kitob miqdorini oshirish yoki kamaytirish yoki o'chirish
 print("Filedagi kitob sonini ID bo'yicha [0] --> ochirish || [1] --> Countini 1 taga kamaytirish")
-x = BOOK(0, "", "", 0, 0)  # Obyekt yaratib, uning metodlarini chaqiramiz
+x = BOOK(0, "", "", 0, 0)  
 if int(input(":")):
     a_id = int(input("Qaysi ID bo'yicha countini kamaytiramiz? "))
     x.countt(a_id)
